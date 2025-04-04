dd625c0b544845eec5e3b0d08c5c89ef80287767
VisualizedConfusion2 <thh005.edu.zealand.dk>
2025-04-04 10:56:52 +0200

    gg

---
+++ IndexModel.cs
@@ -0,0 +1,81 @@
+using Dsv_Project_v3.Models;
+using Dsv_Project_v3.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dsv_Project_v3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RoomServices _rs;
+        private readonly BookingService _bs;
+        [BindProperty]
+        public List<MeetingRoom> Rooms { get; set; }
+
+        public List<MeetingRoomBooking> Bookings { get; set; }
+        List<MeetingRoomBooking> _activeBookings = new List<MeetingRoomBooking>();
+
+        bool _isBooked = false;
+        [BindProperty]
+        public int filterCap { get; set; }
+        [BindProperty]
+        public bool filterWB { get; set; }
+        [BindProperty]
+        public bool filterSB { get; set; }
+
+        private DateTime setDate = DateTime.Now;
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, RoomServices rs, BookingService bs)
+        {
+            _logger = logger;
+            Rooms = rs.GetAll();
+            _rs = rs;
+            Bookings = bs.GetAll();
+            _bs = bs;
+        }
+
+        public void Vacancy()
+        {
+            foreach (MeetingRoomBooking booking in Bookings)
+            {
+                if (booking.StartDateTime.Date == setDate.Date)
+                {
+                    _activeBookings.Add(booking);
+                }
+            }
+        }
+
+        public bool BookChecker(int hour, int roomID)
+        {
+            if (_activeBookings != null)
+            {
+                foreach (var booking in _activeBookings)
+                {
+                    if (hour == booking.StartDateTime.Hour && roomID == booking.RoomID)
+                    {
+                        _isBooked = true;
+                    }
+                    else if (hour == booking.EndDateTime.Hour && roomID == booking.RoomID)
+                    {
+                        _isBooked = false;
+                    }
+                }
+            }
+            return _isBooked;
+        }
+
+        public void OnGet()
+        {
+            Vacancy();
+        }
+
+        public IActionResult OnPost(int idroom)
+        {
+            MeetingRoom bound = _rs.Get(idroom);
+
+            return RedirectToPage("/Form", new { roomname = bound.ID });
+        }
+    }
+}
